71868410ce16016a01eb0045766ac0c49e718b6b
dilungasr <dilungathegreat@gmail.com>
2024-11-08 20:07:28 +0300

    Upgrade Encrypt function to generate and prepend IV to the cipherText

--- encrypt.go
+++ encrypt.go
@@ -7,18 +7,27 @@ import (
 
 // Encrypt encrypts plainPhrase using the given secrete key and initialization vector(iv).
 // Then returns an encrypted base64 URL-encoded string.
-func Encrypt(plainPhrase, secreteKey string, iv []byte) (encryptedPhrase string, err error) {
+func Encrypt(plainPhrase, secreteKey string) (encryptedPhrase string, err error) {
 	//  create aes cipher
 	block, err := aes.NewCipher([]byte(secreteKey))
 	if err != nil {
 		return encryptedPhrase, err
 	}
 
-	// convert the phrase to []bytes
+	// generate IV (initalization vector)
+	IV, err := RandBytes(16)
+	if err != nil {
+		return encryptedPhrase, err
+	}
+
+	// perform encryption
 	plainText := []byte(plainPhrase)
-	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb := cipher.NewCFBEncrypter(block, IV)
 	cipherText := make([]byte, len(plainText))
 	cfb.XORKeyStream(cipherText, plainText)
+
+	// prepend IV to the ciphertext
+	cipherText = append(IV, cipherText...)
 
 	// return the base64Encoded string
 	return base64Encoder(cipherText), err
